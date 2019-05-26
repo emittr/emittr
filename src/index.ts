5821a6fe3ad77d28978e1f5bcc16ecdabe2e9bc7
Brian Faust <hello@basecode.sh>
2019-05-26 07:43:55 +0300

    add listenOnce, add flush and adjust dispatch* arguments

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,12 @@ export interface IEventDispatcher {
 	/**
 	 * Register many listeners with the dispatcher.
 	 */
-	listenMany(events: [EventName, EventListener][]): () => void;
+	listenMany(events: [EventName, EventListener][]): Map<EventName, () => void>;
+
+	/**
+	 * Register a one-time listener with the dispatcher.
+	 */
+	listenOnce(name: EventName, listener: EventListener): void;
 
 	/**
 	 * Remove a listener from the dispatcher.
@@ -20,45 +25,50 @@ export interface IEventDispatcher {
 	/**
 	 * Remove many listeners from the dispatcher.
 	 */
-	forgetMany(event: EventName): void;
+	forgetMany(events: [EventName, EventListener][]): void;
+
+	/**
+	 * Remove all listeners from the dispatcher.
+	 */
+	flush(): void;
 
 	/**
 	 * Get all of the listeners for a given event name.
 	 */
-	getListeners(event?: EventName): EventListener[];
+	getListeners(event: EventName): EventListener[];
 
 	/**
 	 * Determine if a given event has listeners.
 	 */
-	has(event: EventName): boolean;
+	hasListeners(event: EventName): boolean;
 
 	/**
 	 * Fire an event and call the listeners in asynchronous order.
 	 */
-	dispatch(event: EventName): Promise<void>;
+	dispatch<T = any>(event: EventName, data?: T): Promise<void>;
 
 	/**
 	 * Fire an event and call the listeners in sequential order.
 	 */
-	dispatchSeq(event: EventName): Promise<void>;
+	dispatchSeq<T = any>(event: EventName, data?: T): Promise<void>;
 
 	/**
 	 * Fire an event and call the listeners in synchronous order.
 	 */
-	dispatchSync(event: EventName): void;
+	dispatchSync<T = any>(event: EventName, data?: T): void;
 
 	/**
 	 * Fire many events and call the listeners in asynchronous order.
 	 */
-	dispatchMany(events: Record<EventName, EventListener>): Promise<void>;
+	dispatchMany<T = any>(events: [EventName, T][]): Promise<void>;
 
 	/**
 	 * Fire many events and call the listeners in sequential order.
 	 */
-	dispatchManySeq(events: Record<EventName, EventListener>): Promise<void>;
+	dispatchManySeq<T = any>(events: [EventName, T][]): Promise<void>;
 
 	/**
 	 * Fire many events and call the listeners in synchronous order.
 	 */
-	dispatchManySync(events: Record<EventName, EventListener>): void;
+	dispatchManySync<T = any>(events: [EventName, T][]): void;
 }
